75a4469468b412654a4294bbecd2ab26dc261378
David Arinze <fivelanes72@gmail.com>
2024-11-06 14:29:22 +0100

    designed the projects page

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ function toggleDrawer() {
   const drawer = document.getElementById('drawer');
   drawer.classList.toggle('show');
 }
+
+function showProjects(tab) {
+    const ongoingProjects = document.querySelector('.ongoing');
+    const completedProjects = document.querySelector('.completed');
+    const tabButtons = document.querySelectorAll('.tab-button');
+    
+    if (tab === 'ongoing') {
+        ongoingProjects.style.display = 'grid';
+        completedProjects.style.display = 'none';
+    } else {
+        ongoingProjects.style.display = 'none';
+        completedProjects.style.display = 'grid';
+    }
+
+    tabButtons.forEach(button => button.classList.remove('active'));
+    document.querySelector(`.tab-button.${tab}`).classList.add('active');
+}
